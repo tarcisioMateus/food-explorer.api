6ceee9af3ccc6a319215ff7451fc050ff5355ac8
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-20 21:19:38 +0000

    created src/controllers/UsersController.js

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,16 @@
+const UserRepository = require('../repositories/UserRepository')
+const CreateServices = require('../services/user/CreateService')
+
+class UsersController {
+    async create (request, response) {
+        const { name, email, password } = request.body
+
+        const userRepository = new UserRepository()
+        const createServices = new CreateServices(userRepository)
+        await createServices.execute({ name, email, password })
+
+        return response.status(201).json()
+    }
+}
+
+module.exports = UsersController
