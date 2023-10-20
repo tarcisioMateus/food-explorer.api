7f372825a2de519bba5c8f0294cb45cc754cd4d2
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-20 21:33:51 +0000

    updated src/server.js "can handle errors now"

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
+require('express-async-errors')
+
 const express = require('express')
+const routes = require('./routes')
+const AppError = require('./utils/AppError')
 
 const app = express()
+app.use(express.json)
+app.use(routes)
+
+
+app.use(( error, request, response, next ) => {
+    if (error instanceof AppError) {
+        return response.status(error.status).json({
+            message : error.message
+        })
+    }
+    return response.status(500).json({
+        message : `Internal Server Error .......... ${error.message}`,
+    })
+})
 
 const PORT = 3333
 app.listen(PORT, () => console.log(`running on port: ${PORT}`))
