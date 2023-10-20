93c85d42743d6e93dc8cbc85b211a685e36ea640
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-20 21:11:51 +0000

    created src/services/user/CreateService.js

---
+++ src/services/user/CreateService.js
@@ -0,0 +1,24 @@
+const AppError = require('../../utils/AppError')
+const { hash } = require('bcryptjs')
+
+class CreateService {
+    constructor ({ userRepository }) {
+        this.userRepository = userRepository
+    }
+
+    async execute ({ name, email, password }) {
+        const userWithEmail = await this.userRepository.findByEmail({ email })
+
+        if (userWithEmail) {
+            throw new AppError("Email unavailable! ")
+        }
+
+        const cryptedPassword = await hash(password, 8)
+
+        const user = await this.userRepository.create({ name, email, password: cryptedPassword }) 
+
+        return user
+    }
+}
+
+module.exports = CreateService
