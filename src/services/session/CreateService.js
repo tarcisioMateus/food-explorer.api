5ff8334db4469d23699cee41e22e705c5c4a3178
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-24 21:12:50 +0000

    created src/services/session/CreateService.js

---
+++ src/services/session/CreateService.js
@@ -0,0 +1,34 @@
+const AppError = require('../../utils/AppError')
+const authConfig = require("../../configs/auth")
+
+const { compare } = require("bcryptjs")
+const { sign } = require("jsonwebtoken")
+
+class CreateServices {
+
+    constructor({ 
+        userRepository 
+    }) {
+        this.userRepository = userRepository
+    }
+
+    async execute({ email, password }) {
+        const user = await this.userRepository.findByEmail({ email })
+
+        if (!user) throw new AppError('wrong email or password', 401)
+
+        const passwordMatched = await compare(password, user.password)
+
+        if (!passwordMatched) throw new AppError('wrong email or password', 401)
+
+        const { secret, expiresIn } = authConfig.jwt
+        const token = sign({}, secret, {
+        subject: String(user.id),
+        expiresIn
+        })
+
+        return { token, user }
+    }
+}
+
+module.exports = CreateServices
