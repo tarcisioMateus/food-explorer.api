1249f34a4d10f14ae6e356dc051abb4605e7b325
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-24 20:13:14 +0000

    created src/controllers/DishesAvatarController.js

---
+++ src/controllers/DishesAvatarController.js
@@ -0,0 +1,22 @@
+const DiskStorage = require("../providers/DiskStorage")
+const DishRepository = require('../repositories/DishRepository')
+
+const UpdateServices = require('../services/dish/avatar/UpdateService')
+
+class DishesAvatarController { 
+
+    async update(request, response) {
+        const { dish_id } = request.params 
+        const avatarFileName = request.file.filename
+    
+        const updateServices = new UpdateServices ({
+            dishRepository: new DishRepository(), 
+            diskStorage: new DiskStorage()
+        })
+        const dish = await updateServices.execute({ id: dish_id, avatarFileName })
+
+        return response.status(200).json()
+    }
+}
+
+module.exports = DishesAvatarController
--- src/services/dish/avatar/UpdateService.js
+++ src/services/dish/avatar/UpdateService.js
@@ -1,5 +1,3 @@
-const AppError = require("../../../utils/AppError")
-
 class UpdateServices {
     constructor({ 
         dishRepository, diskStorage 
