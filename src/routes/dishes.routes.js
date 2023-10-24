67517ad1be23b6b334674624a711a8ed0f362446
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-24 21:46:04 +0000

    applied  middlewares/ensureAuthenticated.js on dishes.routes.js

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -2,6 +2,8 @@ const { Router } = require('express')
 const multer = require('multer')
 const uploadConfig = require('../configs/upload')
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+
 const DishesController = require('../controllers/DishesController')
 const DishesAvatarController = require('../controllers/DishesAvatarController')
 const dishesController = new DishesController()
@@ -10,6 +12,8 @@ const dishesAvatarController = new DishesAvatarController()
 
 const upload = multer(uploadConfig.MULTER)
 const dishesRoutes = Router()
+
+dishesRoutes.use(ensureAuthenticated)
 
 dishesRoutes.post('/', dishesController.create)
 dishesRoutes.get('/', dishesController.index)
