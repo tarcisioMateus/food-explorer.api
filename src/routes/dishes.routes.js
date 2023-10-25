9988122d1e5008a1069762cf7182435aa1ef3d46
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-25 18:02:52 +0000

    updated src/routes/dishes.routes.js

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,26 +1,17 @@
 const { Router } = require('express')
-const multer = require('multer')
-const uploadConfig = require('../configs/upload')
 
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+const dishesAdminRoutes = require('./dishesAdmin.routes')
 
 const DishesController = require('../controllers/DishesController')
-const DishesAvatarController = require('../controllers/DishesAvatarController')
 const dishesController = new DishesController()
-const dishesAvatarController = new DishesAvatarController()
 
-
-const upload = multer(uploadConfig.MULTER)
 const dishesRoutes = Router()
 
 dishesRoutes.use(ensureAuthenticated)
 
-dishesRoutes.post('/', dishesController.create)
+dishesRoutes.use('/admin', dishesAdminRoutes)
 dishesRoutes.get('/', dishesController.index)
 dishesRoutes.get('/:id', dishesController.show)
-dishesRoutes.put('/:id', dishesController.update)
-dishesRoutes.delete('/:id', dishesController.delete)
-
-dishesRoutes.patch('/avatar/:dish_id', upload.single("avatar"), dishesAvatarController.update)
 
 module.exports = dishesRoutes
