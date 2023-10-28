cd54ac40a3a9a2bdd1ce042a0648b0c546e284f2
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-28 19:05:32 +0000

    updated price input format on services/dish/CreateService and UpdateService

--- src/services/dish/UpdateService.js
+++ src/services/dish/UpdateService.js
@@ -40,8 +40,8 @@ function inputValidation ({ name, description, ingredients, price, category }) {
     if (!name || !description || !ingredients.length || !price || !category ) {
       throw new AppError("Fill in all fields.")
     }
-    if ( !( /R\$[\t ]*((\d{1,3}\.?)+(,\d{2}))/.test(price) ) ) {
-      throw new AppError("Wrong formatted price.")
+    if ( !( /[\t ]*((\d{1,3}\.?)+(,\d{2}))/.test(price) ) ) {
+      throw new AppError("Wrong formatted price, it must be like this: 00,00")
     }
 }
 
