61206a80887ecf9133bbaadce7bb12c8a361d34e
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-31 16:26:53 -0300

    created src/controllers/FavoritesController.js - delete()

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -1,6 +1,7 @@
 const FavoriteRepository = require('../repositories/FavoriteRepository')
 
 const CreateServices = require('../services/favorite/CreateService')
+const DeleteServices = require('../services/favorite/DeleteService')
 
 class DishesController {
 
@@ -16,7 +17,17 @@ class DishesController {
     return response.status(201).json(favorite)
   }
 
-    
+  async delete (request, response) {
+    const { user_id } = request.user.id
+    const { dish_id } = request.params
+
+    const deleteServices = new DeleteServices({
+      favoriteRepository: new FavoriteRepository()
+    })
+    await deleteServices.execute({ user_id, dish_id })
+
+    return response.status(200).json()
+}
 }
 
 module.exports = DishesController
