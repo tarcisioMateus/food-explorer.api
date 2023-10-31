ab7a0e2fd9c8e1d95199fdaaf12305c7ec13d205
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-31 15:59:24 -0300

    created src/services/favorite/CreateService.js

---
+++ src/services/favorite/CreateService.js
@@ -0,0 +1,17 @@
+class CreateServices {
+  constructor({ 
+    favoriteRepository 
+  }) {
+    this.favoriteRepository = favoriteRepository
+  }
+
+  async execute({ 
+    user_id, dish_id 
+  }) {
+    const favorite = await this.favoriteRepository.create({ user_id, dish_id }) 
+    
+    return favorite
+  }
+}
+
+module.exports = CreateServices
