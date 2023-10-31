80c0e883e8aafa4f01c1b957ec84abeb6d9a9b47
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-10-31 16:57:21 -0300

    created src/controllers/FavoritesDishesController.js

--- src/controllers/FavoritesController.js
+++ src/controllers/FavoritesController.js
@@ -2,9 +2,8 @@ const FavoriteRepository = require('../repositories/FavoriteRepository')
 
 const CreateServices = require('../services/favorite/CreateService')
 const DeleteServices = require('../services/favorite/DeleteService')
-const IndexIdServices = require('../services/favorite/dish/IndexIdService')
 
-class DishesController {
+class FavoritesController {
 
   async create (request, response) {
     const { user_id } = request.user.id
@@ -30,16 +29,6 @@ class DishesController {
     return response.status(200).json()
   }
 
-  async indexId (request, response) {
-    const { user_id } = request.user.id
-
-    const indexIdServices = new IndexIdServices({ 
-      favoriteRepository: new FavoriteRepository()
-    })
-    const favoritesDishesId = await indexIdServices.execute({ user_id })
-
-    return response.status(200).json(favoritesDishesId)
-  }
 }
 
-module.exports = DishesController
+module.exports = FavoritesController
---
+++ src/controllers/FavoritesDishesController.js
@@ -0,0 +1,31 @@
+const FavoriteRepository = require('../repositories/FavoriteRepository')
+
+const IndexIdServices = require('../services/favorite/dish/IndexIdService')
+const IndexDataServices = require('../services/favorite/dish/IndexDataService')
+
+class FavoritesDishesController {
+
+  async indexId (request, response) {
+    const { user_id } = request.user.id
+
+    const indexIdServices = new IndexIdServices({ 
+      favoriteRepository: new FavoriteRepository()
+    })
+    const favoritesDishesId = await indexIdServices.execute({ user_id })
+
+    return response.status(200).json(favoritesDishesId)
+  }
+
+  async indexData (request, response) {
+    const { user_id } = request.user.id
+
+    const indexDataServices = new IndexDataServices({ 
+      favoriteRepository: new FavoriteRepository()
+    })
+    const favoritesDishesData = await indexDataServices.execute({ user_id })
+
+    return response.status(200).json(favoritesDishesData)
+  }
+}
+
+module.exports = FavoritesDishesController
