a0f27251b87e203effed08024bfe2ab903c2858d
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-11-15 21:18:48 -0300

    implemented routes/users.routes.js - get /validate

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,13 @@
 const { Router } = require('express')
 
 const UsersController = require('../controllers/UsersController')
+const UsersValidateController = require('../controllers/UsersValidateController')
 const usersController = new UsersController()
+const usersValidateController = new UsersValidateController()
 
 const usersRoutes = Router()
 
 usersRoutes.post('/', usersController.create)
+usersRoutes.get('/validate', usersValidateController.index)
 
 module.exports = usersRoutes
