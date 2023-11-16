227c62981a00e1e7e6d05baa7b67564b824692e3
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-11-15 21:49:25 -0300

    bug fixxed

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require('express')
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+
 const UsersController = require('../controllers/UsersController')
 const UsersValidateController = require('../controllers/UsersValidateController')
 const usersController = new UsersController()
@@ -8,6 +10,6 @@ const usersValidateController = new UsersValidateController()
 const usersRoutes = Router()
 
 usersRoutes.post('/', usersController.create)
-usersRoutes.get('/validate', usersValidateController.index)
+usersRoutes.get('/validated', ensureAuthenticated, usersValidateController.index)
 
 module.exports = usersRoutes
