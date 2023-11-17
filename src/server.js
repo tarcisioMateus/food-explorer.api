7c8ce267f39df249dd872fff31f6e20dd20fef2f
Tarcísio Mateus <tarcisiomateus98@gmail.com>
2023-11-17 17:44:09 -0300

    Update server.js cors origin web

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["https://foodexplorer-api-aflp.onrender.com", "http://localhost:5173"],
+    origin: ["https://foodexplorer-api-aflp.onrender.com", "https://foodexplorerweb.netlify.app"],
     credentials: true
 }))
 
